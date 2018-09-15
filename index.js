8ad69446e1c2800a1d65bfc141a5275b0cc48482
gauravzgit <gauravsharma1428@gmail.com>
2018-09-16 01:51:56 +0530

    first fix

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    req.send({ hi : "there"})
+    res.send({ hi : "there"})
 })
 
-app.listen(8008);
+app.listen(3001);
 
-console.log('Your app is running at http:localhost:8008')
+console.log('Your app is running at port : 3001')
