d6031710b45631f742869b10ab68a243470cbdd4
gauravzgit <gauravsharma1428@gmail.com>
2018-09-15 13:15:33 +0530

    added gitignore and node_modules and new packages

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    req.send({ hi : "there"})
+})
+
+app.listen(8008);
+
+console.log('Your app is running at http:localhost:8008')
